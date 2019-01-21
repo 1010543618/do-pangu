321290872b9273ed050fb52249154081e294baed
zjf <1010543618@qq.com>
2019-01-21 22:02:36 +0800

    Add the node env banner.

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,12 @@
 const pkg = require('./package.json');
 
 export default {
-    input: "./src/index.js",
-    external: Object.keys(pkg.dependencies),
-    output: {
-        file: "./bin/index.js",
-        format: "cjs",
-        sourcemap: true
-    }
+  input: './src/index.js',
+  external: Object.keys(pkg.dependencies),
+  output: {
+    banner: '#!/usr/bin/env node',
+    file: './bin/index.js',
+    format: 'cjs',
+    sourcemap: true,
+  },
 };
